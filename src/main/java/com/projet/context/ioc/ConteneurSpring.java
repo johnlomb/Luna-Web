0e430958e2c5151451d5223ff41c5d3c10c9ce4f
johnlomb <jonpoke@hotmail.fr>
2018-01-11 17:09:55 +0100

    premier commit web.xml,pom et conteneur spring

---
+++ src/main/java/com/projet/context/ioc/ConteneurSpring.java
@@ -0,0 +1,62 @@
+package com.projet.context.ioc;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@PropertySource(value= {"classpath:db.properties"})
+@ComponentScan(basePackages = "com.formation.model")
+@EnableTransactionManagement
+public class ConteneurSpring {
+	
+	@Autowired
+	private Environment env;
+
+
+	@Bean
+	public DataSource getDataSource() {
+		BasicDataSource dataSource=new BasicDataSource();
+		dataSource.setDriverClassName(env.getRequiredProperty("db.driver"));
+		dataSource.setUrl(env.getRequiredProperty("db.url"));
+		dataSource.setUsername(env.getRequiredProperty("db.username"));
+		dataSource.setPassword(env.getRequiredProperty("db.password"));
+		return dataSource;
+	}
+	
+	@Bean
+	public LocalSessionFactoryBean getSessionFactory() {
+		LocalSessionFactoryBean factoryBean=new LocalSessionFactoryBean();
+		factoryBean.setDataSource(getDataSource());
+		
+		Properties properties=new Properties();
+		properties.put(AvailableSettings.SHOW_SQL, env.getRequiredProperty("hibernate.show_sql"));
+		properties.put(AvailableSettings.HBM2DDL_AUTO, env.getRequiredProperty("hibernate.hbm2ddl.auto"));
+		properties.put(AvailableSettings.DIALECT, env.getRequiredProperty("hibernate.dialect"));
+		properties.put(AvailableSettings.STATEMENT_BATCH_SIZE, env.getRequiredProperty("hibernate.batch.size"));
+		properties.put(AvailableSettings.CURRENT_SESSION_CONTEXT_CLASS, env.getRequiredProperty("hibernate.current.session.context.class"));
+		
+		factoryBean.setHibernateProperties(properties);
+		factoryBean.setPackagesToScan(new String[] {"com.formation.model"});
+		return factoryBean;
+	}
+	
+	@Bean
+	public HibernateTransactionManager geTransactionManager(){
+		HibernateTransactionManager transactionManager=new HibernateTransactionManager();
+		transactionManager.setSessionFactory(getSessionFactory().getObject());
+		return transactionManager;
+	}
+}
